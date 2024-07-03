0fddeb75ef27f583d4e6b5e47af66447b673ca37
Georg Stockinger <gstockinger@a2mac1.com>
2024-07-03 07:48:21 +0200

    extract url

--- app/server.c
+++ app/server.c
@@ -1,11 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+
+const char* HTTP_VERSION = "HTTP/1.1";
+
+enum Method {
+    GET,
+    POST,
+};
+
+enum Method parse_method(char* str) {
+    if (strcmp(str, "GET") == 0) {
+        return GET;
+    }
+    else {
+        return POST;
+    }
+}
+
+typedef struct {
+    char* name;
+    char* value;
+} Header;
+
+typedef struct {
+    enum Method method;
+    char* target;
+    char* version;
+    char* headers;
+    char* body;
+} Request;
+
+
+void parse_request(Request* request, char* buffer){
+    int offset = 0;
+    char* method = strtok(buffer, " ");
+    offset += sizeof(method) + 1;
+
+    char* path = strtok(0, " ");
+    offset += sizeof(path) + 1;
+
+    char* version = strtok(0, "\n");
+    offset += sizeof(version) + 1;
+
+    char* header = &buffer[offset - 1];
+    char* p = strstr(header, "\r\n\r\n");
+    int pos = p - header;
+    header[pos] = 0;
+    char* body = &header[pos + 4];
+
+    request->method = parse_method(method);
+    request->version = version;
+    request->target = path;
+    request->headers = header;
+    request->body = body;
+}
+
+typedef struct {
+    int a;
+
+} Response;
+
 
 int main() {
     // Disable output buffering
@@ -57,12 +118,36 @@ int main() {
     int fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
     printf("Client connected\n");
 
-    char *ok = "HTTP/1.1 200 OK\r\n\r\n";
-    int len = strlen(ok);
+    char buffer[1024];
+    int received = recv(fd, &buffer, 1024, 0);
+    if (received == 0) {
+        goto END;
+    }
+    else if (received < 0) {
+        printf("Error receiving data: %s \n", strerror(errno));
+        return 1;
+    }
 
-    int bytes_sent = send(fd, ok, len, 0);
+    Request request = {0};
+    parse_request(&request, &buffer[0]);
 
+    char *reponse;
+    printf("%s\n", request.target);
+    if (strcmp(request.target, "/index.html") == 0) {
+        reponse = "HTTP/1.1 200 OK\r\n\r\n";
+    }
+    else if (strcmp(request.target, "/") == 0) {
+        reponse = "HTTP/1.1 200 OK\r\n\r\n";
+    }
+    else {
+        reponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+    int len = strlen(reponse);
+    int bytes_sent = send(fd, reponse, len, 0);
+
+END:
     close(server_fd);
 
     return 0;
 }
+
